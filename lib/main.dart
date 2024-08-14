1c558a94cfc82f6213c66b26a1bf26ced2211c15
Thitiphong Nimmak <64160048@go.buu.ac.th>
2024-08-14 16:04:50 +0700

    create rount next page

--- lib/main.dart
+++ lib/main.dart
@@ -2,28 +2,45 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class Controller extends GetxController {
-  var count = 0;
+  var count = 0.obs;
   void increment() {
     count++;
-    update();
+    
   }
 }
 
-void main() => runApp(MaterialApp(home: Home()));
+void main() => runApp(GetMaterialApp(home: Home()));
 
 class Home extends StatelessWidget {
   final controller = Get.put(Controller());
   @override
   Widget build(context) => Scaffold(
       appBar: AppBar(title: Text("counter")),
-      body: Center(
-        child: GetBuilder<Controller>(
-            builder: (_) => Text(
-                  'clicks: ${controller.count}',
-                )),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center ,
+        children: [
+          Obx(() => Center(
+            child:  Text(
+                      'clicks: ${controller.count}',
+          ))),
+      
+          ElevatedButton(
+              child: Text('Next Route'),
+              onPressed: () {
+                Get.to(Second());
+              },
+            ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () => controller.increment(),
       ));
+}
+class Second extends StatelessWidget {
+  final Controller ctrl = Get.find<Controller>();
+  @override
+  Widget build(context){
+     return Scaffold(body: Center(child: Text("${ctrl.count}")));
+  }
 }
